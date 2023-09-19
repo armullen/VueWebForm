016fe98a8e0428aee6e5b94d440a9a13c8b3c624
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-19 10:02:40 -0500

    working form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label>Email</label>
         <input type="email" required v-model="email">
 
         <label>Password</label>
         <input type="password" required v-model="password">
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
         <label>Role:</label>
         <select v-model="role">
@@ -17,12 +18,13 @@
         <div v-for="skill in skills" :key="skill" class="pill"> 
             <span @click="deleteSkill(skill)"> {{ skill }}</span>
         </div>
-            <input type="text" v-model="skill">
-
-
+           
         <div class="terms">
             <input type="checkbox" v-model="terms" required>
             <label>Accept terms and conditions</label>
+        </div>
+        <div class="submit">
+            <button>Create an Account</button>
         </div>
 <!-- 
         <div>
@@ -59,7 +61,8 @@
                 terms: false,
                 // names: []
                 tempSkill: '',
-                skills: []
+                skills: [],
+                passwordError: ''
             }
         },
         methods:{
@@ -75,6 +78,18 @@
                     this.skills = this.skills.filter((item) => {
                         return skill !== item;
                     }); 
+            },
+            handleSubmit(){
+                this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 characters';
+                if(!this.passwordError){
+                    alert('Form submitted successfully');
+                    // console.log(this.email, this.password, this.role, this.terms, this.skills)
+                    console.log('email: ', this.email)
+                    console.log('password: ', this.password)
+                    console.log('role: ', this.role)
+                    console.log('terms accepted: ', this.terms)
+                    console.log('skills: ', this.skills)
+                }
             }
         }
     }
@@ -125,6 +140,23 @@ input[type="checkbox"] {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+button{
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit{
+    text-align: center;
+}
+.error{
+    color: red;
+    font-size: 0.8em;
+    margin-top: 10px;
+    text-align: center;
 }
 
 </style>
